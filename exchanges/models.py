17757f3a3624da08f8acef4c16f3cf8ce1a30b60
jeanlcorrea <jeanlucorrea@gmail.com>
2024-06-12 15:30:01 -0300

    feat: add models

--- exchanges/models.py
+++ exchanges/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+from books.models import BookModels
+from exchanges.enums import BookExchangeStatus
+from users.models import UserModels
+
+
+class ExchangeModels(TimeStampedModel):
+    id = models.UUIDField(db_column='ID', primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    book = models.ForeignKey(BookModels, db_column='BOOK', on_delete=models.CASCADE)
+    requester = models.ForeignKey(UserModels, db_column='REQUESTER',
+                                  on_delete=models.CASCADE, related_name='requested_exchanges')
+    owner = models.ForeignKey(UserModels, db_column='OWNER',
+                              on_delete=models.CASCADE, related_name='owner_exchanges')
+    status = models.CharField(db_column='STATUS', max_length=30, choices=BookExchangeStatus.choices())
+
+    class Meta:
+        db_table = 'EXCHANGES'
+        verbose_name = 'EXCHANGE'
+        verbose_name_plural = 'EXCHANGES'
--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,16 @@
+import uuid
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
+from users.models import UserModels
 
-# Create your models here.
+
+class NotificationModels(TimeStampedModel):
+    id = models.UUIDField(db_column='ID', primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    user = models.ForeignKey(UserModels, db_column='USER', on_delete=models.CASCADE)
+    message = models.CharField(db_column='MESSAGE', max_length=500)
+    is_read = models.BooleanField(db_column='IS_READ', default=False)
+
+    class Meta:
+        db_table = 'NOTIFICATIONS'
+        verbose_name = 'NOTIFICATION'
+        verbose_name_plural = 'NOTIFICATIONS'
